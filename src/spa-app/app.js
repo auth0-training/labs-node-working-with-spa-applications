d68d6360536c0fb273111fbb56f31144b05a9475
kazemicode <sara@kazemicode.org>
2023-04-21 11:23:37 -0700

    test

--- src/spa-app/app.js
+++ src/spa-app/app.js
@@ -1,6 +1,7 @@
 import router from "./router";
 
 (async function () {
+  console.log("config");
   const domain = window.env.AUTH0_DOMAIN;
   const clientId = window.env.CLIENT_ID;
   const redirect_uri = window.env.APP_URL;
