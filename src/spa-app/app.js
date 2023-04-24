4ab687712c0dd97ff5da8724ffb2e6679be034f9
Sara Kazemi <36528008+kazemicode@users.noreply.github.com>
2023-04-24 13:40:46 -0700

    Update app.js
    Fix reload issue when logged in

--- src/spa-app/app.js
+++ src/spa-app/app.js
@@ -11,12 +11,13 @@ import router from "./router";
     // avoid calling router twice when handling redirect callback upon sign in
     if (!sessionStorage.getItem("reload")) {
       router();
+      sessionStorage.setItem("reload", "true");
     }
   });
 
   //handle user reload of browser
   if (sessionStorage.getItem("reload")) {
-    await router();
     sessionStorage.setItem("reload", "true");
+    await router();
   }
 })();
